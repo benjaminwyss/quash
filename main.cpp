466418d7ba673d619d4d79229639690d7c0b349c
Jeff Kissick <jkissick@ku.edu>
2019-10-06 18:18:07 -0500

    Added functionality for background jobs, print jobs, and pipes. HOWEVER, we need to hammer out how to parse the & at the end of a command properly without conflicting with other things.  The function works well when I tested it however with & just at the front of commands.  Also likely need to function out the main loop so we can pass in commands for execution rather than rewrite a bunch of code. That or we could throw everything into main.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,19 @@
 std::string path = "";
 std::string home = "";
 char workingDirectory[PATH_MAX];
+int numberOfJobs = 0;
+void printJobs();
+void backgroundJob(std::string);
+void pipe(std::string, std::string);
+//bool contains(std::string, char);
+struct job {
+       int isActive;
+ 	     int jobid;
+	     int pid;
+	     std::string command;
+	   };
+
+std::vector<job> jobs;
 
 int main(int argc, char **argv, char **envp)
 {
@@ -54,6 +67,14 @@ int main(int argc, char **argv, char **envp)
 
       if (commandStream >> command)
       {
+        //NEED TO FIGURE OUT HOW TO PARSE THE '&' WITHOUT BREAKING OTHER PARSING COMMANDS, I SUCK WITH STRSTREAMS
+        //LIKELY NEED TO FUNCTION OUT THIS SECTION TO PASS THE COMMAND THAT NEEDS TO BE EXECUTED FROM PIPE OR
+        //BACKGROUND PROCESS AS WELL, I LEFT EXECUTION BLANK FOR THE TIME BEING IN THOSE FUNCTIONS
+        // std::string holdCommand = command;
+        // if(commandStream >> command && command == '&')
+        // {
+        //  backgroundJob(holdCommand);
+        // }
         if (command == "echo")
         {
           if (commandStream >> command)
@@ -136,15 +157,15 @@ int main(int argc, char **argv, char **envp)
         }
 
         else if (command == "jobs")
-        {
-
+        {		
+		      printJobs();
         }
 
         else if (command == "quit" || command == "exit")
         {
           exit(0);
         }
-
+	
         else
         {
           std::vector<std::string> argsVector;
@@ -208,3 +229,105 @@ int main(int argc, char **argv, char **envp)
 
     return 0;
 }
+
+void printJobs()
+{
+
+
+	std::cout << "[JOBID]  PID  COMMAND\n";
+	int i;
+	for(i = 0; i < numberOfJobs; i++)
+	{
+		std::cout << "[" << jobs[i].jobid << "]" << "  " << jobs[i].pid << "  " << jobs[i].command << "\n";
+	}
+
+}
+
+void backgroundJob(std::string command)
+{
+  pid_t pid; // process id
+  pid_t sid; // session id
+  std::string fullCommand = command + " &";
+  pid = fork(); // create a fork
+
+  if(pid == 0)
+  {
+    sid = setsid(); // set a session id
+
+    if(sid < 0) // if the sid is less than 0 then we the fork failed
+    {
+      std::cout << "The fork failed, oops...\n";
+      exit(EXIT_FAILURE);
+    }
+    else
+    {
+
+      std::cout << "[" << numberOfJobs << "]" << "  " << getpid() << " is running in the background.\n";
+      system(fullCommand.c_str()); // temporary execution to test, this will need to be a full bore execution step
+      std::cout << "[" << numberOfJobs << "]" << "  " << getpid() << " finished " << command << ".\n";
+      exit(0);
+    }
+  }
+  else
+  {
+    jobs.push_back(job());
+    jobs[numberOfJobs].jobid = numberOfJobs;
+    jobs[numberOfJobs].pid = pid;
+    jobs[numberOfJobs].command = command;
+    numberOfJobs++;
+    while(waitpid(pid, NULL, WEXITED|WNOHANG) > 0) 
+    {
+    }
+  }
+  
+}
+
+//void pipe(std::string fullCommand) // if we don't end up splitting the string through the stream use this
+
+// Assuming we stream in arguments delimitted by white space we can use this version
+void pipe(std::string leftCommand, std::string rightCommand)
+{
+  int fds[2]; // just like in lab...
+  //std::string copyFull = fullCommand; // probably not needed
+  //std::string commandLeft = strtok(copyFull, '|'); // just put the full command in here instead?
+  //std::string commandRight = strtok(NULL, "\n"); // There might be a better function to use to break apart the command
+
+  pid_t pid1;
+  pid_t pid2;
+
+  pid1 = fork();
+  if(pid1 == 0)
+  {
+    dup2(fds[1], STDOUT_FILENO);
+    close(fds[0]);
+    int num = 0;
+    // execute leftCommand here again, need to either make the main loop a function to pass this or find a work around
+    exit(0);
+  }
+
+  pid2 = fork();
+  if(pid2 == 0)
+  {
+    dup2(fds[0], STDIN_FILENO);
+    close(fds[0]);
+    int num = 0;
+    //execute rightCommand here
+  }
+  close(fds[0]);
+  close(fds[1]);
+
+}
+
+// bool contains(std::string s, char c)
+// {
+//   bool isFound = false;
+//   for(int i = 0; i < s.length(); i++)
+//   {
+//     if(s[i] == c)
+//     {
+//       isFound = true;
+//       break;
+//     }
+//   }
+//   return isFound;
+// }
